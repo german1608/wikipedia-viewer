bcea3908adc3eac3acf281e4cac307a5b6769ed1
German Robayo Paz <germanrobayo33@gmail.com>
2017-06-28 23:02:41 -0400

    Add some files

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&titles=Stack%20Overflow API for specific quote
+
+// https://en.wikipedia.org/wiki/Special:Random API For random quote
+
+
